eab44ac0d9463e8687ec9a933c26ca59041f48ba
raftagia <40564336+raftagia@users.noreply.github.com>
2020-11-04 16:09:13 +0100

    Added all files

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
-# SlimLine-webpages
- 
+# Pagine web SlimLine
+
+Queste sono le pagine web per i dispositivi SlimLine.
+All'interno del pacchetto sono contenuti titti i file necessari allo sviluppo.
+
+## Componenti richiesti
+
+I componenti necessari allo sviluppo delle pagine sono elencati nel file
+
+	package.json
+
+Ma una lista è disponibile qui sotto:
+
+1. **Bulma** un framework css.
+2. **Ionicons** libreria minimale icone.
+3. **Sfw** file di gestione comunicazione dalle pagine allo SlimLine
+
+
+## Sviluppo
+
+Per sviluppare le pagine è necessario usare la cartella:
+
+	src -> cartella generale sviluppo
+		js -> cartella js
+			index.js -> file js generico
+			sfw******* -> file di funzionamento nelle pagine
+		sass -> cartella sass
+			index.scss -> sovrascrittura variabili globali e inclusionì	
+			elsist.scss -> file di stile personalizzato
+
+Tutti gli output saranno generati in:
+
+	dist -> cartella generale sviluppo
+		scripts -> inclusioni scripts
+			js -> cartella js
+				index.js -> file js totale
+			sass -> cartella sass
+				index.css -> css totale
+			
+### Inizio progetto, download dipendenze e compilazioni
+
+Per scaricare tutte le dipendenze occorre
+
+	npm install
+
+JS Per compilare tutti i file *.js e generarne uno unico viene usato terser.
+
+	npm run terser
+
+SCSS per compilare il foglio di stile **index.css** viene usato node sass
+
+	npm run css-build
+
+## Funzionamento
+
+**Importazione variabili**
+
+	import { read_data, send_data, data_page } from '../scripts/js/index.js';
+
+**Per leggere i valori non appena la pagina è stata caricata:**
+
+	read_data( data_page, setup_values );
+
+**Aggiornamento a tempo:**
+
+	setInterval(function(){ read_data( data_page, setup_values ); }, 1000);
+
+**Per inviare un form**
+
+- **id**: "#MyForm"
+- **this** è l'oggetto DOM del form,
+- **data_page** è il nome della pagina htm contentente i dati, di default viene presa dallo stesso nome del file HTML,
+- **setup_value** è la funzione di valorizzazione dati.
+
+-
+
+	document.querySelector('#MyForm').addEventListener( 'submit', function(event) {
+		event.preventDefault();
+		send_data(this, data_page, setup_values );
+	})
+
+**setup_values**, serve per scrivere negli oggetti html il testo di risposta dal json arrivato nella variabile response.
+
+	<script type="module">
+
+		// Importazioni.
+
+		import { read_data, send_data, data_page } from '../scripts/js/index.js';
+
+		read_data( data_page, setup_values );
+		setInterval(function(){ read_data( data_page, setup_values ); }, 1000);
+
+		// Funzione di settaggio variabili
+	
+		document.querySelector('#MyForm').addEventListener( 'submit', function(event) {
+			event.preventDefault();
+			send_data(this, "", data_page, setup_values );
+		})
+
+		// Funzione di visualizzazione variabili.
+
+		function setup_values( response ) {
+			document.getElementById( "system" ).innerHTML = response.system;
+			document.getElementById( "status" ).innerHTML = response.status;
+			document.getElementById( "result" ).innerHTML = response.result;
+		}
+
+	</script>
+
---
+++ src/js/sfw191a000.js
@@ -0,0 +1,153 @@
+/**
+ * SFW191A020
+ * 
+ * Le seguenti funzioni gestiscono lo standard AJAX "Asynchronous Java and XML",
+ * con esse viene gestito lo scambio dinamico di dati con le pagine web.
+ *
+ * La funzione "read_data" eseguita ciclicamente invia le richieste al
+ * server eseguendo lo script "AJAXMng.php".
+ *
+ * La funzione "AJAXHandleRsp" viene eseguita ad ogni cambiamento di stato della
+ * richiesta AJAX. Nella funzione si controlla lo stato per determinare se
+ * operazione terminata e per gestire stringa di risposta ricevuta dal server.
+ */
+
+// Messaggio di benvenuto.
+
+console.log("%c Welcome to SlimLine webpages ! ","color: #303030; font-family:monospace; background-color:#009933; font-size: 14px; font-weight:bold;");
+
+// Inizializzazione variabili.
+
+export var data_page = window.location.pathname.split('/')[2].split(".")[0] + '.htm';
+var fct_address; // Funzione da eseguire per valorizzazione elementi DOM.
+var semaforo; // Variabile semaforica.
+
+/**
+ * Funzione read_data(page_data, fct)
+ * 
+ * Questa funzione invia la richiesta AJAX al server. Viene richiesta la pagina
+ * definita in "Page".
+ *
+ * @param string page_data pagina sulla quale inviare le richieste (.htm).
+ * @param function fct nome della funzione da eseguire al callback.
+ */
+export function read_data( page_data, fct ) {
+    
+    // Oggetto richiesta
+
+    const XHR = new XMLHttpRequest();
+    fct_address = fct;
+
+    // Test semaforo
+
+    if (semaforo) return;
+    semaforo = true;
+
+    /**
+     * Creo variabile random per generare ad ogni richiesta un valore diverso in
+     * modo da evitare che il browser avendo sempre la stessa richiesta usi la
+     * cache e non la invii. Alternativa a questo è di utilizzare il metodo POST
+     * che non viene mai memorizzato in cache.
+    */
+
+    var Rnd=Math.random();
+
+    // Invio richiesta allo script php su server di gestione. Il nome metodo "GET" in maiuscolo, alcuni browser sono case sensitive.
+
+    if (XHR != null) {
+        XHR.open('GET', page_data+'?Rnd='+escape(Rnd), true);
+        XHR.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+
+        // Definisco funzione di gestione risposta ed invio richiesta. Nel send occorre sempre specificare "null".
+
+        XHR.onreadystatechange = function () { check_function( XHR.readyState, XHR.responseText, false ); };
+        XHR.send(null);
+    }
+}
+
+/**
+ * Funzione sendData( form_element, data_page )
+ * 
+ * Funzione di invio dati del form, questa funzione prende in input dove mandare il form,
+ * ovvero la pagina valori (.htm) e l'oggetto dom del form. da questo oggetto,
+ * la funzione stessa potrà accedere a tutti gli input del form e leggerne i valori per costruire il corpo del POST.
+ */
+export function send_data( form_element, data_page, fct ) {
+
+    fct_address = fct;
+
+    // Var data.
+    var data = {};
+
+    // Loop per tutti gli oggetti del form.
+    
+    var i;
+    for (i = 0; i < form_element.length ;i++) {
+      data[form_element.elements[i].name] = form_element.elements[i].value;
+    }
+    console.log(data);
+
+
+    const XHR = new XMLHttpRequest();
+
+    let urlEncodedData = "",
+        urlEncodedDataPairs = [],
+        name;
+
+    // Turn the data object into an array of URL-encoded key/value pairs.
+    for( name in data ) {
+        urlEncodedDataPairs.push( encodeURIComponent( name ) + '=' + encodeURIComponent( data[name] ) );
+    }
+
+    // Combine the pairs into a single string and replace all %-encoded spaces to 
+    // the '+' character; matches the behaviour of browser form submissions.
+    urlEncodedData = urlEncodedDataPairs.join( '&' ).replace( /%20/g, '+' );
+
+    // Define what happens on successful data submission
+    XHR.addEventListener( 'load', function(event) {
+        check_function( XHR.readyState, XHR.responseText, true );
+    } );
+
+    // Define what happens in case of error
+    XHR.addEventListener( 'error', function(event) {
+        alert( 'Oops! Something went wrong.' );
+    } );
+
+    // Set up our request
+    XHR.open( 'POST', data_page );
+
+    // Add the required HTTP header for form data POST requests
+    XHR.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded;charset=utf-8' );
+
+    // Finally, send our data.
+    XHR.send( urlEncodedData );
+}
+
+/**
+ * Funzione check_function()
+ * 
+ * Funzione che esegue controlli basilari sullo stato di risposta e semaforo.
+ * In caso si possa procedere chiama la funzione di valorizzazione variabili definita nella pagina,
+ * che è stata passata per indirizzo su "fct_address" passando ad essa il corpo di della risposta.
+ */
+function check_function( status, response, display_messages = false ) {
+
+    // Se il codice di risposta è diverso da 4 allora non faccio nulla.
+
+    if ( 4 != status ) return;
+
+    // Controllo semaforo.
+
+    semaforo = false;
+
+    // Controllo presenza di errori
+
+    var response = JSON.parse( response );
+    if( response.hasOwnProperty('result') && display_messages ){
+        alert( response.result );
+    }
+
+    // Chiamo funzione.
+
+    fct_address( response );
+}
