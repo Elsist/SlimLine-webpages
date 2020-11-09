49a592df1b0d211a64cd0752160cd9fd56711a21
raftagia <40564336+raftagia@users.noreply.github.com>
2020-11-09 15:00:15 +0100

    Added webpack
    Added webpack and now we can compile the bunble with it.
    Webpack now bundle all js in single file and css in single file.

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+	entry: {
+		bundle: './src/js/index.js',
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'dist/scripts/js'),
+	},
+	module: {
+		rules: [{
+			test: /\.scss$/,
+			use: [ MiniCssExtractPlugin.loader, { loader: 'css-loader' }, { loader: 'sass-loader'} ]
+		}]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+		  filename: '../css/bundle.css'
+		}),
+	]
+};
