70ae43e723c24800a870b8b4761085ac4191375f
raftagia <40564336+raftagia@users.noreply.github.com>
2020-11-27 17:21:44 +0100

    Added protocols and security page and other bug fixes

--- src/js/pages_functions.js
+++ src/js/pages_functions.js
@@ -92,7 +92,7 @@ export function index_page() {
 export function general_page() {
 
 	read_data( data_page, false, display_value );
-	setInterval(function(){ read_data( data_page, true, display_value ); }, 1000);
+	window._els_interval = setInterval(function(){ read_data( data_page, true, display_value ); }, 1000);
 
 	// Funzioni settaggio variabili.
 
@@ -140,6 +140,7 @@ export function general_page() {
 		document.getElementById( "OSID_DAYLIGHT_ZONE_input" ).value = response.OSID_DAYLIGHT_ZONE;
 
 		// Il plc torna secondi e devo convertirli in RFC3339 per il valore html5 dell'input.
+		
 		document.getElementById( "OSID_LOCAL_DATETIME_input" ).value = jsdate_to_rfc3339( new Date( response.OSID_LOCAL_DATETIME * 1000 ) );
 
 		// System settings.
@@ -283,13 +284,15 @@ export function servers_page() {
 		document.getElementById( "OSID_FTP_SVR_PORT_view" ).innerHTML = response.OSID_FTP_SVR_PORT;
 		document.getElementById( "OSID_FTP_SVR_PASSIVE_PORT_view" ).innerHTML = response.OSID_FTP_SVR_PASSIVE_PORT;
 		document.getElementById( "OSID_IPSERVER0_PORT_view" ).innerHTML = response.OSID_IPSERVER0_PORT;
-		document.getElementById( "OSID_IPSERVER0_MODE_view" ).innerHTML = response.OSID_IPSERVER0_MODE;
+
+		document.getElementById( "OSID_IPSERVER0_MODE_view" ).innerHTML = ( response.OSID_IPSERVER0_MODE == 0 ) ? 'TCP' : (( response.OSID_IPSERVER0_MODE == 1 ) ? 'UDP' : '??' );
+		document.getElementById( "OSID_IPSERVER1_MODE_view" ).innerHTML = ( response.OSID_IPSERVER1_MODE == 0 ) ? 'TCP' : (( response.OSID_IPSERVER1_MODE == 1 ) ? 'UDP' : '??' );
+		document.getElementById( "OSID_IPSERVER2_MODE_view" ).innerHTML = ( response.OSID_IPSERVER2_MODE == 0 ) ? 'TCP' : (( response.OSID_IPSERVER2_MODE == 1 ) ? 'UDP' : '??' );
+
 		document.getElementById( "OSID_IPSERVER0_CNN_view" ).innerHTML = response.OSID_IPSERVER0_CNN;
 		document.getElementById( "OSID_IPSERVER1_PORT_view" ).innerHTML = response.OSID_IPSERVER1_PORT;
-		document.getElementById( "OSID_IPSERVER1_MODE_view" ).innerHTML = response.OSID_IPSERVER1_MODE;
 		document.getElementById( "OSID_IPSERVER1_CNN_view" ).innerHTML = response.OSID_IPSERVER1_CNN;
 		document.getElementById( "OSID_IPSERVER2_PORT_view" ).innerHTML = response.OSID_IPSERVER2_PORT;
-		document.getElementById( "OSID_IPSERVER2_MODE_view" ).innerHTML = response.OSID_IPSERVER2_MODE;
 		document.getElementById( "OSID_IPSERVER2_CNN_view" ).innerHTML = response.OSID_IPSERVER2_CNN;
 
 		// On - Off labels
@@ -370,5 +373,250 @@ export function servers_page() {
 		document.getElementById( "OSID_IPSERVER2_PORT_input" ).value = response.OSID_IPSERVER2_PORT;
 		document.getElementById( "OSID_IPSERVER2_CNN_input" ).value = response.OSID_IPSERVER2_CNN;
 		
+	}
+}
+
+/**
+ * Function protocols_page()
+ * 
+ * Funzione gestione pagina protocols
+ */
+export function protocols_page() {
+
+	read_data( data_page, false, display_value );
+
+	// Funzioni di settaggio variabili.
+
+	document.querySelector('#MODBUS0').addEventListener( 'submit', function(event) {
+		event.preventDefault();
+		send_data(this, data_page, display_value );
+	})
+	document.querySelector('#MODBUS1').addEventListener( 'submit', function(event) {
+		event.preventDefault();
+		send_data(this, data_page, display_value );
+	})
+	document.querySelector('#MODBUS2').addEventListener( 'submit', function(event) {
+		event.preventDefault();
+		send_data(this, data_page, display_value );
+	})
+	document.querySelector('#MODBUS3').addEventListener( 'submit', function(event) {
+		event.preventDefault();
+		send_data(this, data_page, display_value );
+	})
+	document.querySelector('#MODBUS4').addEventListener( 'submit', function(event) {
+		event.preventDefault();
+		send_data(this, data_page, display_value );
+	})
+	document.querySelector('#MODBUS5').addEventListener( 'submit', function(event) {
+		event.preventDefault();
+		send_data(this, data_page, display_value );
+	})
+
+	// Funzione di visualizzazione variabili.
+
+	function display_value( response, on_loop ) {
+
+		// Functions.
+
+		function modbus_type( value ){
+			switch ( value ) {
+				case 0: return( 'ASCII' ); break;
+				case 1: return( 'RTU' ); break;
+				case 2: return( 'OVER IP' ); break;
+				default: return( '??' );
+			}
+		}
+		function modbus_device( value ){
+			switch ( value ) {
+				case 0: return( 'COM 0' ); break;
+				case 1: return( 'COM 1' ); break;
+				case 2: return( 'COM 2' ); break;
+				case 100: return( 'IP SERVER [0]' ); break;
+				case 101: return( 'IP SERVER [1]' ); break;
+				case 102: return( 'IP SERVER [2]' ); break;
+				default: return( '??' );
+			}
+		}
+
+		// Display value in view.
+	
+		document.getElementById( "OSID_MODBUS_SVR0_NODE_view" ).innerHTML = response.OSID_MODBUS_SVR0_TYPE;
+		document.getElementById( "OSID_MODBUS_SVR0_TYPE_view" ).innerHTML = modbus_type( response.OSID_MODBUS_SVR0_TYPE );
+		document.getElementById( "OSID_MODBUS_SVR0_DEVICE_view" ).innerHTML = modbus_device( response.OSID_MODBUS_SVR0_DEVICE );
+		document.getElementById( "OSID_MODBUS_SVR1_NODE_view" ).innerHTML = response.OSID_MODBUS_SVR1_TYPE;
+		document.getElementById( "OSID_MODBUS_SVR1_TYPE_view" ).innerHTML = modbus_type( response.OSID_MODBUS_SVR1_TYPE );
+		document.getElementById( "OSID_MODBUS_SVR1_DEVICE_view" ).innerHTML = modbus_device( response.OSID_MODBUS_SVR1_DEVICE );
+		document.getElementById( "OSID_MODBUS_SVR2_NODE_view" ).innerHTML = response.OSID_MODBUS_SVR2_TYPE;
+		document.getElementById( "OSID_MODBUS_SVR2_TYPE_view" ).innerHTML = modbus_type( response.OSID_MODBUS_SVR2_TYPE );
+		document.getElementById( "OSID_MODBUS_SVR2_DEVICE_view" ).innerHTML = modbus_device( response.OSID_MODBUS_SVR2_DEVICE );
+		document.getElementById( "OSID_MODBUS_SVR3_NODE_view" ).innerHTML = response.OSID_MODBUS_SVR3_TYPE;
+		document.getElementById( "OSID_MODBUS_SVR3_TYPE_view" ).innerHTML = modbus_type( response.OSID_MODBUS_SVR3_TYPE );
+		document.getElementById( "OSID_MODBUS_SVR3_DEVICE_view" ).innerHTML = modbus_device( response.OSID_MODBUS_SVR3_DEVICE );
+		document.getElementById( "OSID_MODBUS_SVR4_NODE_view" ).innerHTML = response.OSID_MODBUS_SVR4_TYPE;
+		document.getElementById( "OSID_MODBUS_SVR4_TYPE_view" ).innerHTML = modbus_type( response.OSID_MODBUS_SVR4_TYPE );
+		document.getElementById( "OSID_MODBUS_SVR4_DEVICE_view" ).innerHTML = modbus_device( response.OSID_MODBUS_SVR4_DEVICE );
+		document.getElementById( "OSID_MODBUS_SVR5_NODE_view" ).innerHTML = response.OSID_MODBUS_SVR5_TYPE;
+		document.getElementById( "OSID_MODBUS_SVR5_TYPE_view" ).innerHTML = modbus_type( response.OSID_MODBUS_SVR5_TYPE );
+		document.getElementById( "OSID_MODBUS_SVR5_DEVICE_view" ).innerHTML = modbus_device( response.OSID_MODBUS_SVR5_DEVICE );
+
+		// On - Off labels
+
+		if ( Boolean( response.OSID_MODBUS_SVR0_EN ) ) {
+			document.getElementById("OSID_MODBUS_SVR0_EN_view").classList.add('is-success');
+			document.getElementById("OSID_MODBUS_SVR0_EN_view").classList.remove('is-normal');
+		} else {
+			document.getElementById("OSID_MODBUS_SVR0_EN_view").classList.remove('is-success');
+			document.getElementById("OSID_MODBUS_SVR0_EN_view").classList.add('is-normal');
+		}
+
+		if ( Boolean( response.OSID_MODBUS_SVR1_EN ) ) {
+			document.getElementById("OSID_MODBUS_SVR1_EN_view").classList.add('is-success');
+			document.getElementById("OSID_MODBUS_SVR1_EN_view").classList.remove('is-normal');
+		} else {
+			document.getElementById("OSID_MODBUS_SVR1_EN_view").classList.remove('is-success');
+			document.getElementById("OSID_MODBUS_SVR1_EN_view").classList.add('is-normal');
+		}
+
+		if ( Boolean( response.OSID_MODBUS_SVR2_EN ) ) {
+			document.getElementById("OSID_MODBUS_SVR2_EN_view").classList.add('is-success');
+			document.getElementById("OSID_MODBUS_SVR2_EN_view").classList.remove('is-normal');
+		} else {
+			document.getElementById("OSID_MODBUS_SVR2_EN_view").classList.remove('is-success');
+			document.getElementById("OSID_MODBUS_SVR2_EN_view").classList.add('is-normal');
+		}
+
+		if ( Boolean( response.OSID_MODBUS_SVR3_EN ) ) {
+			document.getElementById("OSID_MODBUS_SVR3_EN_view").classList.add('is-success');
+			document.getElementById("OSID_MODBUS_SVR3_EN_view").classList.remove('is-normal');
+		} else {
+			document.getElementById("OSID_MODBUS_SVR3_EN_view").classList.remove('is-success');
+			document.getElementById("OSID_MODBUS_SVR3_EN_view").classList.add('is-normal');
+		}
+
+		if ( Boolean( response.OSID_MODBUS_SVR4_EN ) ) {
+			document.getElementById("OSID_MODBUS_SVR4_EN_view").classList.add('is-success');
+			document.getElementById("OSID_MODBUS_SVR4_EN_view").classList.remove('is-normal');
+		} else {
+			document.getElementById("OSID_MODBUS_SVR4_EN_view").classList.remove('is-success');
+			document.getElementById("OSID_MODBUS_SVR4_EN_view").classList.add('is-normal');
+		}
+
+		if ( Boolean( response.OSID_MODBUS_SVR5_EN ) ) {
+			document.getElementById("OSID_MODBUS_SVR5_EN_view").classList.add('is-success');
+			document.getElementById("OSID_MODBUS_SVR5_EN_view").classList.remove('is-normal');
+		} else {
+			document.getElementById("OSID_MODBUS_SVR5_EN_view").classList.remove('is-success');
+			document.getElementById("OSID_MODBUS_SVR5_EN_view").classList.add('is-normal');
+		}
+
+		// Display value in inputs.
+
+		document.getElementById("OSID_MODBUS_SVR0_EN_input").value = response.OSID_MODBUS_SVR0_EN;
+		document.getElementById( "OSID_MODBUS_SVR0_NODE_input" ).value = response.OSID_MODBUS_SVR0_TYPE;
+		document.getElementById( "OSID_MODBUS_SVR0_TYPE_input" ).value = response.OSID_MODBUS_SVR0_TYPE;
+		document.getElementById( "OSID_MODBUS_SVR0_DEVICE_input" ).value = response.OSID_MODBUS_SVR0_DEVICE;
+		document.getElementById("OSID_MODBUS_SVR1_EN_input").value = response.OSID_MODBUS_SVR1_EN;
+		document.getElementById( "OSID_MODBUS_SVR1_NODE_input" ).value = response.OSID_MODBUS_SVR1_TYPE;
+		document.getElementById( "OSID_MODBUS_SVR1_TYPE_input" ).value = response.OSID_MODBUS_SVR1_TYPE;
+		document.getElementById( "OSID_MODBUS_SVR1_DEVICE_input" ).value = response.OSID_MODBUS_SVR1_DEVICE;
+		document.getElementById("OSID_MODBUS_SVR2_EN_input").value = response.OSID_MODBUS_SVR2_EN;
+		document.getElementById( "OSID_MODBUS_SVR2_NODE_input" ).value = response.OSID_MODBUS_SVR2_TYPE;
+		document.getElementById( "OSID_MODBUS_SVR2_TYPE_input" ).value = response.OSID_MODBUS_SVR2_TYPE;
+		document.getElementById( "OSID_MODBUS_SVR2_DEVICE_input" ).value = response.OSID_MODBUS_SVR2_DEVICE;
+		document.getElementById("OSID_MODBUS_SVR3_EN_input").value = response.OSID_MODBUS_SVR3_EN;
+		document.getElementById( "OSID_MODBUS_SVR3_NODE_input" ).value = response.OSID_MODBUS_SVR3_TYPE;
+		document.getElementById( "OSID_MODBUS_SVR3_TYPE_input" ).value = response.OSID_MODBUS_SVR3_TYPE;
+		document.getElementById( "OSID_MODBUS_SVR3_DEVICE_input" ).value = response.OSID_MODBUS_SVR3_DEVICE;
+		document.getElementById("OSID_MODBUS_SVR4_EN_input").value = response.OSID_MODBUS_SVR4_EN;
+		document.getElementById( "OSID_MODBUS_SVR4_NODE_input" ).value = response.OSID_MODBUS_SVR4_TYPE;
+		document.getElementById( "OSID_MODBUS_SVR4_TYPE_input" ).value = response.OSID_MODBUS_SVR4_TYPE;
+		document.getElementById( "OSID_MODBUS_SVR4_DEVICE_input" ).value = response.OSID_MODBUS_SVR4_DEVICE;
+		document.getElementById("OSID_MODBUS_SVR5_EN_input").value = response.OSID_MODBUS_SVR5_EN;
+		document.getElementById( "OSID_MODBUS_SVR5_NODE_input" ).value = response.OSID_MODBUS_SVR5_TYPE;
+		document.getElementById( "OSID_MODBUS_SVR5_TYPE_input" ).value = response.OSID_MODBUS_SVR5_TYPE;
+		document.getElementById( "OSID_MODBUS_SVR5_DEVICE_input" ).value = response.OSID_MODBUS_SVR5_DEVICE;
+	}
+}
+
+/**
+ * Function security_page()
+ * 
+ * Funzione gestione pagina security
+ */
+export function security_page() {
+
+	read_data( data_page, false, display_value );
+
+	// Funzioni di settaggio variabili.
+
+	document.querySelector('#SECURITY').addEventListener( 'submit', function(event) {
+		event.preventDefault();
+		send_data(this, data_page, display_value );
+	})
+
+	// Funzione di visualizzazione variabili.
+
+	function display_value( response, on_loop ) {
+
+		// Display value in inputs.
+		var i;
+		for ( i=0; i <= 7; i++ ) {
+			document.getElementById("OSID_SECUR_DIR_" + i).value = response["OSID_SECUR_DIR_" + i];
+			document.getElementById( "OSID_SECUR_DIR_RD_" + i ).value = response["OSID_SECUR_DIR_RD_" + i];
+			document.getElementById( "OSID_SECUR_DIR_WR_" + i ).value = response["OSID_SECUR_DIR_WR_" + i];
+		}
+
+	}
+}
+
+/**
+ * Function userinfos_page()
+ * 
+ * Funzione gestione pagina user infos
+ */
+export function userinfos_page() {
+
+	read_data( data_page, false, display_value );
+
+	// Funzioni di settaggio variabili.
+
+	document.querySelector('#SYSUSET').addEventListener( 'submit', function(event) {
+		event.preventDefault();
+		send_data(this, data_page, display_value );
+	})
+	document.querySelector('#PROTECTIONCODES').addEventListener( 'submit', function(event) {
+		event.preventDefault();
+		send_data(this, data_page, display_value );
+	})
+
+	// Funzione di visualizzazione variabili.
+
+	function display_value( response, on_loop ) {
+
+		document.getElementById( "OSID_PLC_CFG_INFO0" ).innerHTML = response.OSID_PLC_CFG_INFO0;
+		document.getElementById( "OSID_PLC_CFG_INFO1" ).innerHTML = response.OSID_PLC_CFG_INFO1;
+		document.getElementById( "OSID_PLC_CFG_INFO2" ).innerHTML = response.OSID_PLC_CFG_INFO2;
+		document.getElementById( "OSID_PLC_CFG_INFO3" ).innerHTML = response.OSID_PLC_CFG_INFO3;
+		document.getElementById( "OSID_PLC_CFG_SET0_view" ).innerHTML = response.OSID_PLC_CFG_SET0;
+		document.getElementById( "OSID_PLC_CFG_SET1_view" ).innerHTML = response.OSID_PLC_CFG_SET1;
+		document.getElementById( "OSID_PLC_CFG_SET2_view" ).innerHTML = response.OSID_PLC_CFG_SET2;
+		document.getElementById( "OSID_PLC_CFG_SET3_view" ).innerHTML = response.OSID_PLC_CFG_SET3;
+		document.getElementById( "OSID_PLC_PROTECT_CODE0_view" ).innerHTML = response.OSID_PLC_PROTECT_CODE0;
+		document.getElementById( "OSID_PLC_PROTECT_CODE1_view" ).innerHTML = response.OSID_PLC_PROTECT_CODE1;
+		document.getElementById( "OSID_PLC_PROTECT_CODE2_view" ).innerHTML = response.OSID_PLC_PROTECT_CODE2;
+		document.getElementById( "OSID_PLC_PROTECT_CODE3_view" ).innerHTML = response.OSID_PLC_PROTECT_CODE3;
+		
+		// Display value in inputs.
+
+		document.getElementById( "OSID_PLC_CFG_SET0_input" ).value = response.OSID_PLC_CFG_SET0;
+		document.getElementById( "OSID_PLC_CFG_SET1_input" ).value = response.OSID_PLC_CFG_SET1;
+		document.getElementById( "OSID_PLC_CFG_SET2_input" ).value = response.OSID_PLC_CFG_SET2;
+		document.getElementById( "OSID_PLC_CFG_SET3_input" ).value = response.OSID_PLC_CFG_SET3;
+		document.getElementById( "OSID_PLC_PROTECT_CODE0_input" ).value = response.OSID_PLC_PROTECT_CODE0;
+		document.getElementById( "OSID_PLC_PROTECT_CODE1_input" ).value = response.OSID_PLC_PROTECT_CODE1;
+		document.getElementById( "OSID_PLC_PROTECT_CODE2_input" ).value = response.OSID_PLC_PROTECT_CODE2;
+		document.getElementById( "OSID_PLC_PROTECT_CODE3_input" ).value = response.OSID_PLC_PROTECT_CODE3;
+
+
 	}
 }
