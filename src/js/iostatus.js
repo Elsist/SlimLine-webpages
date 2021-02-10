c4602b2544fb8c2b8f84508f80f5e0b9381a15bd
Gianluca <graftacco@elsist.net>
2021-02-10 16:56:20 +0100

    Added 2 missing CPU from io_status page

--- src/js/iostatus.js
+++ src/js/iostatus.js
@@ -149,7 +149,9 @@ var card_ios =[	{Name:"MPS054****", Regex: "MPS054([A-Z0-9]{4})", Input:2, Outpu
 				{Name:"PCB124*500", Regex: "PCB124([A-Z]{1})500", Input:24, Output:24},
 				{Name:"PCB124*600", Regex: "PCB124([A-Z]{1})600", Input:32, Output:16},
 				{Name:"PCB126****", Regex: "PCB126([A-Z0-9]{4})", Input:0, Output:0},
-			 	{Name:"PCB129****", Regex: "PCB129([A-Z0-9]{4})", Input:16, Output:8},			 
+				{Name:"PCB129****", Regex: "PCB129([A-Z0-9]{4})", Input:16, Output:8},			 
+				{Name:"PCB137****", Regex: "PCB137([A-Z0-9]{4})", Input:2, Output:2},
+				{Name:"MPS141****", Regex: "MPS141([A-Z0-9]{4})", Input:6, Output:4},		 
 ];
 
 
